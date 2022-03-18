f6943c577f50d26c37b2e2dcccc9228852401f6d
AlamDK8 <alamdwikurnia@students.amikom.ac.id>
2022-03-18 21:55:08 +0700

    Insialisasi Calculator

---
+++ Calculator.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		
+		static int Penambahan(int a, int b)
+		{
+			return a + b;
+		}
+		
+		static int Pengurangan (int a, int b)
+		{
+			return a + b;
+		}
+		
+		static int Perkalian (int a, int b)
+		{
+			return a * b;
+		}
+		
+		static Pembagian (int a, int b)
+		{
+			return a / b:
+		}
+	}
+}

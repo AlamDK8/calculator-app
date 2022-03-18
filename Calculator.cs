bd84669e5f58ff627671e18d3dba6fad9b4c77a4
AlamDK8 <alamdwikurnia@students.amikom.ac.id>
2022-03-18 22:27:12 +0700

    Calculator stable v1.0

--- Calculator.cs
+++ Calculator.cs
@@ -10,6 +10,26 @@ namespace CalculatorConsoleApp
 	{
 		static void Main(string[] args)
 		{
+			Console.Title = "Applikasi Kalkulator";
+			
+			// int a = 10; // deklarasi variabel a, dengan nilai awal 10;
+			// int b = 6; // deklarasi variabel b, dengan nilai awal 6;
+			
+			Console.Write("Inputkan nilai a = ");
+			int a = int.Parse(Console.ReadLine()); //Proses casting
+			
+			Console.Write("Inputkan nilai b = "); 
+			int b = Convert.ToInt32(Console.ReadLine());//Proses casting
+			
+			Console.WriteLine(); //tambahan bari kosong
+			
+			Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+			Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+			Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+			Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+			
+			Console.WriteLine("\nTekan sembarang key untuk keluar");
+			Console.Readkey();
 		}
 		
 		static int Penambahan(int a, int b)
